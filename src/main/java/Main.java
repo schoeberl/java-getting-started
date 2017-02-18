e24eef8962f76f840667684c51b9395392042d95
Martin Schoeberl <martin@jopdesign.com>
2017-02-18 16:33:06 +0100

    Add a counter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,16 +10,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+// import static javax.measure.unit.SI.KILOGRAM;
+// import javax.measure.quantity.Mass;
+// import org.jscience.physics.model.RelativisticModel;
+// import org.jscience.physics.amount.Amount;
+
 import static spark.Spark.*;
 
 public class Main {
+
+  static int cnt = 0;
 
   public static void main(String[] args) {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> "Hello World from Martin: "+ (++cnt));
+
+
+
+
+
+
+//    get("/hello", (req, res) -> {
+//      RelativisticModel.select();
+//      Amount<Mass> m = Amount.valueOf("12 GeV").to(KILOGRAM);
+//      return "E=mc^2: 12 GeV = " + m.toString();
+//    });
 
     get("/", (request, response) -> {
         Map<String, Object> attributes = new HashMap<>();
